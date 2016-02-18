8df74a8cf78d5df83569476b1d53e3fec34105c3
VenelinGP <venelin.petkov@netcoms.eu>
2016-02-18 21:56:27 +0200

    Added validation in SingUp view.

--- NSQuiz/platforms/android/src/main/assets/app/views/login/signUpView.js
+++ NSQuiz/platforms/android/src/main/assets/app/views/login/signUpView.js
@@ -2,30 +2,34 @@ var view = require("ui/core/view");
 var buttonModule = require("ui/button");
 var observable = require("data/observable");
 
-function onLoad(args) {
-    var page = args.object;
+var pageModules = {
+        onLoad: function (args) {
 
-    var nameTextField = view.getViewById(page, "name");
-    var emailTextField = view.getViewById(page, "email");
-    var passwordTextField = view.getViewById(page, "password");
-    var signUpButton = view.getViewById(page, "signUpButton");
-    var resultLabel = view.getViewById(page, "result");
+       
+        var page = args.object;
+
+        var nameTextField = view.getViewById(page, "name");
+        var emailTextField = view.getViewById(page, "email");
+        var passwordTextField = view.getViewById(page, "password");
+        var signUpButton = view.getViewById(page, "signUpButton");
+        var resultLabel = view.getViewById(page, "result");
 
 
-    signUpButton.on(buttonModule.Button.tapEvent, function () {
+        signUpButton.on(buttonModule.Button.tapEvent, function () {
 
-        var result = "";
+            var result = "";
 
-        if(nameTextField.text === "" || emailTextField.text === "" || passwordTextField.text === ""){
-            result = "Error: All fields required";
-            console.log(result);
+            if(nameTextField.text === "" || emailTextField.text === "" || passwordTextField.text === ""){
+                result = "Error: All fields required";
+                console.log(result);
+            }
+            else{
+                result = "Success!!!\nName: " + nameTextField.text + "\nEmail:" + emailTextField.text + "\nPassword: " + passwordTextField.text;
+                console.log(result);
+            resultLabel.text = result;
         }
-        else{
-            result = "Success!!!\nName: " + nameTextField.text + "\nEmail:" + emailTextField.text + "\nPassword: " + passwordTextField.text;
-            console.log(result);
-        resultLabel.text = result;
-        }
-    });
+        });
+    }
 }
 
-exports.loadSignUpView = onLoad;
+exports.loadSignUpView = pageModules.onLoad;
