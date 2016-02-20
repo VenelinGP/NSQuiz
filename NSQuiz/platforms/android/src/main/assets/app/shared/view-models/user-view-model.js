2c9e5c88883d813b1114a6a30364a70ca00593ab
Peter Velkov <kidroca@gmail.com>
2016-02-20 20:06:44 +0200

    automatic changes to platform from running the tns build  command

--- NSQuiz/platforms/android/src/main/assets/app/shared/view-models/user-view-model.js
+++ NSQuiz/platforms/android/src/main/assets/app/shared/view-models/user-view-model.js
@@ -1,97 +1,88 @@
 "use strict";
-var config = require("../../shared/config");
 var Observable = require("data/observable").Observable;
 var validator = require("email-validator");
 
+var webApi = require("../../shared/data/web-api-service");
+
 function User(info) {
-	info = info || {};
+    info = info || {};
 
-	// You can add properties to observables on creation
-	var viewModel = new Observable({
-		username: info.username || "",
-		email: info.email || "",
-		password: info.password || ""
-	});
+    // You can add properties to observables on creation
+    var viewModel = new Observable({
+        username: info.username || "",
+        email: info.email || "",
+        password: info.password || ""
+    });
 
-	viewModel.login = function() {
-		return fetch(config.apiUrl + "oauth/token", {
-			method: "POST",
-			body: JSON.stringify({
-				username: viewModel.get("username"),
-				password: viewModel.get("password"),
-				grant_type: "password"
-			}),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		.then(handleErrors)
-		.then(function(response) {
-			return response.json();
-		}).then(function(data) {
-			config.token = data.Result.access_token;
-		});
-	};
+    viewModel.login = function () {
+        var user = {
+            username: viewModel.get("username"),
+            password: viewModel.get("password"),
+            grant_type: "password"
+        };
 
-	viewModel.register = function() {
-		return fetch(config.apiUrl + "Users", {
-			method: "POST",
-			body: JSON.stringify({
-				Username: viewModel.get("username"),
-				Email: viewModel.get("email"),
-				Password: viewModel.get("password")
-			}),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		.then(handleErrors);
-	};
+        return webApi.login(user)
+            .then(handleErrors)
+            .then(function (response) {
+                return response.json();
+            });
+    };
 
-	viewModel.resetPassword = function() {
-		return fetch(config.apiUrl + "Users/resetpassword", {
-			method: "POST",
-			body: JSON.stringify({
-				Email: viewModel.get("email"),
-			}),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		.then(handleErrors);
-	};
+    viewModel.register = function () {
+        var user = {
+            username: viewModel.get("username"),
+            password: viewModel.get("password"),
+            confirmPassword: viewModel.get("password")
+        };
 
-	viewModel.isValidUsername = function () {
-		var name = this.get("username");
-		if (name.length < 4) {
-			return false;
-		}
-		return true;
-	};
+        return webApi.register(user);
+    };
 
-	viewModel.isValidEmail = function() {
-		var email = this.get("email");
-		return validator.validate(email);
-	};
+    // Todo: Reset Password Endpoint
+    //viewModel.resetPassword = function() {
+    //	return fetch(config.apiUrl + "Users/resetpassword", {
+    //		method: "POST",
+    //		body: JSON.stringify({
+    //			Email: viewModel.get("email"),
+    //		}),
+    //		headers: {
+    //			"Content-Type": "application/json"
+    //		}
+    //	})
+    //	.then(handleErrors);
+    //};
 
-	viewModel.isValidPassword = function () {
-		var password = this.get("password");
-		console.log(password);
-		if (password.length < 7) {
-			return false;
-		}
-		return true;
-	};
+    viewModel.isValidUsername = function () {
+        var name = this.get("username");
+        if (name.length < 4) {
+            return false;
+        }
+        return true;
+    };
 
-	return viewModel;
+    viewModel.isValidEmail = function () {
+        var email = this.get("email");
+        return validator.validate(email);
+    };
+
+    viewModel.isValidPassword = function () {
+        var password = this.get("password");
+        console.log(password);
+        if (password.length < 7) {
+            return false;
+        }
+        return true;
+    };
+
+    return viewModel;
 }
 
 function handleErrors(response) {
-	if (!response.ok) {
-		console.log(JSON.stringify(response));
-		throw Error(response.statusText);
-	}
-	return response;
+    if (!response.ok) {
+        console.log(JSON.stringify(response));
+        throw Error(response.statusText);
+    }
+    return response;
 }
 
 module.exports = User;
