e9456b0050a762fed99c54cc958a8ad44c1a4496
Peter Velkov <kidroca@gmail.com>
2016-02-21 14:25:27 +0200

    received quiz object as object on the solving page

--- NSQuiz/app/views/quizList/quizList.js
+++ NSQuiz/app/views/quizList/quizList.js
@@ -1,7 +1,23 @@
 "use strict";
 
+var errorHandler = require('../../shared/utils/error-handler');
+var quiz = {};
+
 function pageLoaded(args) {
 	var page = args.object;
+	quiz = page.navigationContext;
+
+	console.log('page loaded');
+	console.log('Quiz for solving: %s', quiz.title);
 }
 
-exports.pageLoaded = pageLoaded;
+function onNavigatedTo(args) {
+	console.log('navigated to	');
+}
+
+var pageObject = {
+	pageLoaded: pageLoaded,
+	onNavigatedTo: onNavigatedTo
+};
+
+module.exports = pageObject;
